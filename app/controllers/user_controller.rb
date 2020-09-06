bf33da0c27147c5955219f567183661561bcb968
ayu0819 <ayutateishi0819@gmail.com>
2020-09-07 01:57:02 +0900

    login and css add

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,65 @@
 class UserController < ApplicationController
+  def login_form
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def mypage
+  end
+
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    # 保存が成功したかどうかで条件分岐をしてください
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/user/#{@user.id}")
+    else
+      render("user/new")
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました"
+      redirect_to("/user/#{@user.id}")
+    else
+      render("user/edit")
+    end
+  end
+
   def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました"
+      # リダイレクト先
+      redirect_to("/")
+    else
+      # @error_messageを定義してください
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      
+      # @emailと@passwordを定義してください
+      @email = params[:email]
+      @password = params[:password]
+      
+      render("user/login_form")
+    end
   end
 
-  def signin
-  end
-
-  def user
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get "login" => "user#login_form"
+  post "login" => "user#login"
+  post "user/:id/update" => "user#update"
+  get "user/:id/edit" => "user#edit"
+  post "user/create" => "user#create"
+  get "user/index" => "user#index"
+  get "user/:id" => "user#show"
   get 'skill/home'
-  get 'user/login'
-  get 'user/signin'
+  get "signup" => "user#new"
   get 'user/mypage'
   get "/" => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
